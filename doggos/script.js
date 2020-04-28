9633eb3971279ec52de1d2522d543d360bf70758
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-28 13:50:27 -0400

    Initial commit

---
+++ doggos/script.js
@@ -0,0 +1,51 @@
+const button = document.querySelector('button') 
+button.addEventListener('click', handleClick)
+
+function handleClick(e) {
+  button.textContent = "Generating doggo..."
+  $.get('https://dog.ceo/api/breeds/image/random')
+    .then((data) => {
+    $("#myImage").attr('src', data.message)
+    const imageDiv = document.querySelector('#images') 
+    doggoImage = document.createElement('img');
+    doggoImage.setAttribute('src', data.message)
+    doggoImage.setAttribute('src', data.message)
+    doggoImage.style.width = "60%";
+    imageDiv.appendChild(doggoImage)
+    button.textContent = "Generate doggo"
+  });
+}
+
+$(document).ready(function() {
+  $.get('https://dog.ceo/api/breeds/list')
+    .then((data) => {
+      const breeds = data.message;
+      const select = document.getElementById('dog-select');
+      breeds.forEach((breed) => {
+        console.log(breed);
+        const option = document.createElement('option');
+        option.value = breed;
+        option.textContent = breed;
+        select.appendChild(option);
+      });
+    });
+});
+
+const select = document.querySelector('select') 
+select.addEventListener('change', handleChange)
+
+function handleChange(e) {
+  // const breedName = this.value
+  $.get(`https://dog.ceo/api/breed/${this.value}/images/random`)
+    .then((data) => {
+    $("#myImage").attr('src', data.message)
+    const imageDiv = document.querySelector('#images') 
+    imageDiv.innerHTML = "";
+    doggoImage = document.createElement('img');
+    doggoImage.setAttribute('src', data.message)
+    doggoImage.setAttribute('src', data.message)
+    doggoImage.style.width = "60%";
+    imageDiv.appendChild(doggoImage)
+    // button.textContent = "Generate doggo"
+  });
+}
