35104299edf2f6fcc19760d00265b536435fb618
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-29 11:48:01 -0400

    Successfully used Promise.all

--- Promises/script.js
+++ Promises/script.js
@@ -6,11 +6,19 @@ var urls = [
   'https://dog.ceo/api/breed/eskimo/images/random'
 ];
 
+// Part 1 - .then() chaining
+// function getUrls(array) {
+//   array.forEach(url => {
+//     $.get(url)
+//       .then(console.log(`data was fetched for ${url}!`))
+//   })
+// }
+
+// Part 2 - Promise.all()
 function getUrls(array) {
-  array.forEach(url => {
-    $.get(url)
-      .then(console.log(`data was fetched for ${url}!`))
-  })
+  Promise.all(array).then(url => console.log("all the data was fetched!"))
 }
+
+// Part 3 - Resolve Reject
 
 getUrls(urls);
